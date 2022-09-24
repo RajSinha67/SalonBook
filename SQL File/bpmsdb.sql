511b4e125f36124a921d7688fb45a00b0d884b4b
Harshit <raj.harshitsinha08@gmail.com>
2022-09-24 18:43:03 +0530

    Adding all files to SalonBook

---
+++ SQL File/bpmsdb.sql
@@ -0,0 +1,113 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` char(50) DEFAULT NULL,
+  `UserName` char(50) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Harshit', 'admin', 7898799798, 'tester1@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', '2019-07-25 06:21:50');
+
+
+CREATE TABLE `tblappointment` (
+  `ID` int(10) NOT NULL,
+  `AptNumber` varchar(80) DEFAULT NULL,
+  `Name` varchar(120) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `PhoneNumber` bigint(11) DEFAULT NULL,
+  `AptDate` varchar(120) DEFAULT NULL,
+  `AptTime` varchar(120) DEFAULT NULL,
+  `Services` varchar(120) DEFAULT NULL,
+  `ApplyDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `Remark` varchar(250) NOT NULL,
+  `Status` varchar(50) NOT NULL,
+  `RemarkDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `tblappointment` (`ID`, `AptNumber`, `Name`, `Email`, `PhoneNumber`, `AptDate`, `AptTime`, `Services`, `ApplyDate`, `Remark`, `Status`, `RemarkDate`) VALUES
+(1, '261064124', 'Komal', 'komal@gmail.com', 7798797897, '7/27/2019', '4:00pm', '1', '2019-07-26 04:48:25', 'Accepted', '1', '2019-07-26 06:41:16'),
+(2, '985645887', 'Kashish', 'Kash@gmail.com', 4654654654, '7/29/2019', '4:30pm', 'Deluxe Pedicure', '2019-07-26 05:04:38', 'Rejected', '2', '2019-07-26 06:47:04'),
+(3, '965887988', 'Sanjeeta Jain', 'sna@gmail.com', 5646464646, '8/20/2019', '2:30pm', 'Loreal Hair Color(Full)', '2019-08-19 12:35:30', 'we will wait', '1', '2019-08-19 13:37:39'),
+(4, '578797544', 'Anuj Kumar', 'phpgurukulofficial@gmail.com', 123456789, '8/30/2019', '1:30am', 'Test', '2019-08-21 16:13:13', 'yes you can visit', '1', '2019-09-10 12:40:20');
+
+
+CREATE TABLE `tblcustomers` (
+  `ID` int(10) NOT NULL,
+  `Name` varchar(120) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(11) DEFAULT NULL,
+  `Gender` enum('Female','Male','Transgender') DEFAULT NULL,
+  `Details` mediumtext,
+  `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `tblcustomers` (`ID`, `Name`, `Email`, `MobileNumber`, `Gender`, `Details`, `CreationDate`, `UpdationDate`) VALUES
+(1, 'Sunita Verma', 'verma@gmail.com', 5546464646, 'Female', 'Taking Hair Spa', '2019-07-26 11:09:10', '2019-07-31 15:15:54'),
+(2, 'Rahul Singh', 'singh@gmail.com', 5565565656, 'Male', 'Taken haircut by him', '2019-07-26 11:10:02', NULL),
+(3, 'Khusbu', 'saini@gmail.com', 4646445464, 'Female', 'khjhhkjkjkuhj', '2019-07-26 11:10:28', NULL);
+
+
+CREATE TABLE `tblpage` (
+  `ID` int(10) NOT NULL,
+  `PageType` varchar(200) DEFAULT NULL,
+  `PageTitle` mediumtext,
+  `PageDescription` mediumtext,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `UpdationDate` date DEFAULT NULL,
+  `Timing` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`, `Timing`) VALUES
+(1, 'aboutus', 'About Us', '        Our main focus is on quality and hygiene. Our Parlour is well equipped with advanced technology equipments and provides best quality services. Our staff is well trained and experienced, offering advanced services in Skin, Hair and Body Shaping that will provide you with a luxurious experience that leave you feeling relaxed and stress free. The specialities in the parlour are, apart from regular bleachings and Facials, many types of hairstyles, Bridal and cine make-up and different types of Facials &amp; fashion hair colourings.', NULL, NULL, NULL, ''),
+(2, 'contactus', 'Contact Us', 'Avalahalli, Yelahanka, Bengaluru', 'salonbookinfo@gmail.com', 9999999999, NULL, '10:30 am to 7:30 pm');
+
+
+
+CREATE TABLE `tblservices` (
+  `ID` int(10) NOT NULL,
+  `ServiceName` varchar(200) DEFAULT NULL,
+  `Cost` int(10) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `tblservices` (`ID`, `ServiceName`, `Cost`, `CreationDate`) VALUES
+(1, 'O3 Facial', 1200, '2019-07-25 11:22:38'),
+(2, 'Fruit Facial', 500, '2019-07-25 11:22:53'),
+(3, 'Charcol Facial', 1000, '2019-07-25 11:23:10'),
+(4, 'Deluxe Menicure', 500, '2019-07-25 11:23:34'),
+(5, 'Deluxe Pedicure', 600, '2019-07-25 11:23:47'),
+(6, 'Normal Menicure', 300, '2019-07-25 11:24:01'),
+(7, 'Normal Pedicure', 400, '2019-07-25 11:24:19'),
+(8, 'U-Shape Hair Cut', 250, '2019-07-25 11:24:38'),
+(9, 'Layer Haircut', 550, '2019-07-25 11:24:53'),
+(10, 'Rebonding', 3999, '2019-07-25 11:25:08');
+
+
+
+ALTER TABLE `tbladmin` ADD PRIMARY KEY (`ID`);
+ALTER TABLE `tbladmin` MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE `tblappointment` ADD PRIMARY KEY (`ID`);
+ALTER TABLE `tblappointment` MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+ALTER TABLE `tblcustomers` ADD PRIMARY KEY (`ID`);
+ALTER TABLE `tblcustomers` MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+ALTER TABLE `tblpage` ADD PRIMARY KEY (`ID`);
+ALTER TABLE `tblpage` MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `tblservices` ADD PRIMARY KEY (`ID`);
+ALTER TABLE `tblservices` MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+
+COMMIT;
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+      <div class="container">
+       <a class="navbar-brand" href="index.php"><h1>SalonBook</h1></a> 
+       
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active"><a href="index.php" class="nav-link"><h4><b>Home</b></h4></a></li>
+            <li class="nav-item"><a href="services.php" class="nav-link"><h4><b>Services</b></h4></a></li>
+            
+            
+            <li class="nav-item"><a href="admin/index.php" class="nav-link"><h4><b>Admin</b></h4></a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
